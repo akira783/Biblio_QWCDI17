ecadea879ea6d553883587e4c0731fab05a558ed
walou <walou@DESKTOP-PFC8CFK.api2000.fr>
2017-03-02 13:34:25 +0100

    test1

--- [FX]Biblio/src/application/Main.java
+++ [FX]Biblio/src/application/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
 		BorderPane root = new BorderPane();
 		Scene scene = new Scene(root,400,400);
 	 
-		 
+		 System.out.println("def");
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
