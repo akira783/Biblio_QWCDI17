469635d50aa750dad69c35cfe7ccf348799a876b
walou <walou@DESKTOP-PFC8CFK.api2000.fr>
2017-03-02 14:24:36 +0100

    classe Personne

--- [FX]Biblio/src/Biblio/Model/Personne.java
+++ [FX]Biblio/src/Biblio/Model/Personne.java
@@ -16,4 +16,36 @@ public class Personne
 		this.dateNaissance = dateNaissance;
 		this.sexe = sexe;
 	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public Date getDateNaissance() {
+		return dateNaissance;
+	}
+
+	public void setDateNaissance(Date dateNaissance) {
+		this.dateNaissance = dateNaissance;
+	}
+
+	public String getSexe() {
+		return sexe;
+	}
+
+	public void setSexe(String sexe) {
+		this.sexe = sexe;
+	}
 }
