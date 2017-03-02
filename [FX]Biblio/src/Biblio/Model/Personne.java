5c32f3a7b1629e6d7f7b6e254e8845eb2bd02654
walou <walou@DESKTOP-PFC8CFK.api2000.fr>
2017-03-02 14:10:47 +0100

    class personne

---
+++ [FX]Biblio/src/Biblio/Model/Personne.java
@@ -0,0 +1,14 @@
+package Biblio.Model;
+
+import java.util.Date;
+
+public class Personne 
+{
+	private String	nom;
+	private String	prenom;
+	private Date	dateNaissance;
+	private String	sexe;
+	
+	
+	
+}
