711c0039573c54407c44c1fa5bd0e5b3d1152900
walou <walou@DESKTOP-PFC8CFK.api2000.fr>
2017-03-02 14:42:27 +0100

    test

--- [FX]Biblio/src/Biblio/Model/Utilisateur.java
+++ [FX]Biblio/src/Biblio/Model/Utilisateur.java
@@ -1,8 +1,19 @@
 package Biblio.Model;
 
-public class Utilisateur 
+import java.util.Date;
+
+public class Utilisateur extends Personne
 {
 	private int	idUtilisateur;
 	private	String	pwd;
 	private	String	pseudonyme;
+	
+	public Utilisateur(String nom, String prenom, Date dateNaissance, String sexe, int idUtilisateur, String pwd,
+			String pseudonyme) 
+	{
+		super(nom, prenom, dateNaissance, sexe);
+		this.idUtilisateur = idUtilisateur;
+		this.pwd = pwd;
+		this.pseudonyme = pseudonyme;
+	}
 }
