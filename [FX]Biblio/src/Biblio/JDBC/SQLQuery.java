d027b42c951827e4577ac504eac7e3a75bbb2fa1
walou <walou@DESKTOP-PFC8CFK.api2000.fr>
2017-03-06 16:53:33 +0100

    te

--- [FX]Biblio/src/Biblio/JDBC/SQLQuery.java
+++ [FX]Biblio/src/Biblio/JDBC/SQLQuery.java
@@ -14,34 +14,42 @@ public class SQLQuery
 	private  Connection conn = JDBC.Connect();;	
 	private String nomUtilisateur, pwd;
 	
-	public boolean checkLog(String idUtil, String pwd)
-	{
-		System.out.println("id :"+idUtil);
-		System.out.println("pwd :"+pwd);
-		System.out.println("Requête en cours...");
-		try {
+	public boolean checkLog(String idUtil, String pwd){
+		 
+		 System.out.println("id :"+idUtil);
+		 System.out.println("pwd :"+pwd);
+		 System.out.println("Requête en cours...");
+		 try {
 			Statement st = conn.createStatement();
+			
 			ResultSet resultat = st.executeQuery("SELECT idUtilisateur, pwd FROM Utilisateur WHERE idUtilisateur = '"+idUtil+"'");
-			while(resultat.next())
-				{
-					 String id = resultat.getString( "idUtilisateur" );
-		        	 if(id.equals(idUtil)){
-						 System.out.println("Trouvé !");
-						 nomUtilisateur = resultat.getString("idUtilisateur");
-						 this.pwd=resultat.getString("pwd");	 				 
-		        	 	}				 
-				}			 
+			 
+			 while(resultat.next()){
+				 
+				 String id = resultat.getString( "idUtilisateur" );
+	             	           
+				 if(id.equals(idUtil)){
+					 System.out.println("Trouvé !");
+					 nomUtilisateur = resultat.getString("idUtilisateur");
+					 this.pwd=resultat.getString("pwd");	 				 
+				 }				 
+			 }			 
 			 if(nomUtilisateur==null) System.out.println("Cette utilisateur n'éxiste pas");
 			 else{				 
 				 System.out.println(nomUtilisateur);			 
 			 }
+			 
 			 if(this.pwd.equals(pwd)){				 
 				 System.out.println("Connection : AUTORISÉE");	
 				 return true;
 			 }			 
 			 else System.out.println("Connection : REFUSÉE");	
 			 return false;
+			 
+			 
+			
 		 } 
+			 
 		 catch (java.lang.NullPointerException e) {
 			 System.out.println("nullptexcept");
 			}
@@ -59,24 +67,33 @@ public class SQLQuery
 			}
 		 }
 		return false;
+		 
 }
 	 
 	 public void setEmpruntEnCours(String pickUpDate ,String idUtilisateur ,String idExemplaire){
+		 
 		 checkStatusExemplaire(Integer.parseInt(idExemplaire));
+		 
 		 try {
 			Statement st = conn.createStatement();
 			st.executeUpdate("INSERT INTO biblio.empruntencours (dateEmprunt,idUtilisateur,idExemplaire) VALUES ('"+pickUpDate+"',"+idUtilisateur+","+idExemplaire+");");
+			
+		
+			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	 
 	 }
+
 	 
 	 public  String checkStatusExemplaire(int idExemplaire){
+		 
+		 
 		 try {
 				Statement st = conn.createStatement();
 				ResultSet resultat = st.executeQuery("SELECT status FROM exemplaire WHERE idExemplaire = '"+idExemplaire+"'");
-		
 				while (resultat.next()){
 				String status = resultat.getString("status");
 				return status;
@@ -86,23 +103,39 @@ public class SQLQuery
 				e.printStackTrace();
 			}
 		return null;
+		 
+		 
+		 
+		 
+		 
 	 }
 	 
 public  void setStatusExemplaire(int i, int idExemplaire){
 		String status = null;
-
-		if(i==1)
+	
+		 if(i==1) {
 			  status = "DISPONIBLE";
-		 if(i==0)
+					 }
+		 
+		 if(i==0){
 			  status = "PRETE";
-		 if(i== -1)
+		 }
+		 
+		 if(i== -1){
 			 status = "SUPPRIME";
+		 }
+		 
+		 
 		 try {
 				Statement st = conn.createStatement();
 				st.executeUpdate("UPDATE exemplaire SET status = '"+status+"' WHERE idExemplaire = "+idExemplaire+";");
+				
+				
 			} catch (SQLException e) {
+				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+		 
 	 }
 
 	public void gereEmpruntSql(ObservableList<EmpruntEnCours> listEmprunt, FenGereEmprunt emprunt)
@@ -123,37 +156,8 @@ public  void setStatusExemplaire(int i, int idExemplaire){
 			emprunt.getList().setItems(listEmprunt);
 				
 		} catch (SQLException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
-	public int getNbEmprunt(int idUtilisateur){
-		
-			int i = 0;
-			Statement st;
-			try {				
-				st = conn.createStatement();			
-				ResultSet resultat = st.executeQuery("SELECT idUtilisateur FROM empruntencours WHERE idUtilisateur = '"+idUtilisateur+"'");
-				System.out.println("Requete getNbEmprunt : OK");
-				while(resultat.next())
-					{					
-					String id = resultat.getString( "idUtilisateur" );
-					i++;					
-					}
-			
-			System.out.println("L'utilisateur : "+ idUtilisateur +" à emprunté "+i+ " livre(s)");
-		
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			return i;
-			
-		
-		
-		
-	}
 }
-	
-	
-
