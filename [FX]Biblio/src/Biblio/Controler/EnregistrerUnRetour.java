77267880f41358a0ec86cc7882115e5313a1120e
walou <walou@DESKTOP-PFC8CFK.api2000.fr>
2017-03-07 11:50:48 +0100

    requete sql pour enregistrer un retour

--- [FX]Biblio/src/Biblio/Controler/EnregistrerUnRetour.java
+++ [FX]Biblio/src/Biblio/Controler/EnregistrerUnRetour.java
@@ -5,8 +5,11 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import Biblio.JDBC.JDBC;
+import Biblio.Model.EmpruntArchive;
 import Biblio.View.FenEnregistrerUnRetour;
 import javafx.event.EventHandler;
 import javafx.scene.Group;
@@ -45,21 +48,42 @@ public class EnregistrerUnRetour
 
 			@Override
 			public void handle(MouseEvent event) {
-				EnregitrerUnEmprunt();
+				EnregistrerUnEmprunt();
 				System.out.println("Enregistrer les infos de retour dans la bibliotheque");
 			}
 		});
 	}
 	
-	public void EnregitrerUnEmprunt()
+	public void EnregistrerUnEmprunt()
 	{
-		try {
-			Statement st = conn.createStatement();
-			ResultSet result = st.executeQuery("SELECT idEmprunt FROM empruntencours WHERE idUtilisateur = "+ Integer.parseInt(retour.getAdherant().getText()) +" AND idExemplaire = "+ Integer.parseInt(retour.getLivre().getText())+";");
-			while(result.next())
-			System.out.println(result.getString("idEmprunt"));
-		} catch (SQLException e) {
-				System.out.println("fail");
-		}
+		String timeStamp = new SimpleDateFormat("yyyy-MM-dd").format(new Date());								
+		
+		
+			Statement st;
+			ResultSet result;
+			String idemprunt = "";
+			
+			try {
+				EmpruntArchive empruntArchive = new EmpruntArchive();
+				st = conn.createStatement();
+				result = st.executeQuery("SELECT * FROM empruntencours WHERE idUtilisateur = "+ Integer.parseInt(retour.getAdherant().getText()) +" AND idExemplaire = "+ Integer.parseInt(retour.getLivre().getText())+";");
+				while(result.next())
+					{
+							empruntArchive = new EmpruntArchive(timeStamp, result.getString("dateEmprunt"), Integer.parseInt(result.getString("idExemplaire")), Integer.parseInt(result.getString("idUtilisateur")));
+							idemprunt = result.getString("idEmprunt");
+					}
+				st.executeUpdate("insert into biblio.empruntarchive (dateEmprunt, dateRestitutionEff, idExemplaire, idUtilisateur)"+ 
+								"VALUES ('" + empruntArchive.getDateEmprunt() + "'," +
+								"'" + empruntArchive.getDateRestitutionEff() +
+								"', " + empruntArchive.getIdExemplaire() +
+								", " + empruntArchive.getIdUtlisateur()+ ");");
+				st.executeUpdate("UPDATE exemplaire SET status = 'DISPONIBLE' where idExemplaire = " + empruntArchive.getIdExemplaire() + ";");
+				st.executeUpdate("DELETE FROM empruntencours where idEmprunt = " + idemprunt + ";");
+				
+	
+			} catch (SQLException e) {
+				
+			}
+		
 	}
 }
--- [FX]Biblio/src/Biblio/Model/EmpruntArchive.java
+++ [FX]Biblio/src/Biblio/Model/EmpruntArchive.java
@@ -4,28 +4,56 @@ import java.util.Date;
 
 public class EmpruntArchive 
 {
-	private Date	dateRestitutionEff;
-	private Date	dateEmprunt;
+	private String	dateRestitutionEff;
+	private String	dateEmprunt;
+	private int		idUtlisateur;
+	private int		idExemplaire;
 	
-	public EmpruntArchive(Date dateRestitutionEff, Date dateEmprunt) 
+	public EmpruntArchive()
+	{
+		
+		
+	}
+	
+	public EmpruntArchive(String dateRestitutionEff, String dateEmprunt, int idExemplaire, int idUtlisateur) 
 	{
 		this.dateRestitutionEff = dateRestitutionEff;
 		this.dateEmprunt = dateEmprunt;
+		this.idUtlisateur = idUtlisateur;
+		this.idExemplaire = idExemplaire;
 	}
 
-	public Date getDateRestitutionEff() {
+	public String getDateRestitutionEff() {
 		return dateRestitutionEff;
 	}
 
-	public void setDateRestitutionEff(Date dateRestitutionEff) {
+	public void setDateRestitutionEff(String dateRestitutionEff) {
 		this.dateRestitutionEff = dateRestitutionEff;
 	}
 
-	public Date getDateEmprunt() {
+	public String getDateEmprunt() {
 		return dateEmprunt;
 	}
 
-	public void setDateEmprunt(Date dateEmprunt) {
+	public void setDateEmprunt(String dateEmprunt) {
 		this.dateEmprunt = dateEmprunt;
 	}
+
+	public int getIdUtlisateur() {
+		return idUtlisateur;
+	}
+
+	public void setIdUtlisateur(int idUtlisateur) {
+		this.idUtlisateur = idUtlisateur;
+	}
+
+	public int getIdExemplaire() {
+		return idExemplaire;
+	}
+
+	public void setIdExemplaire(int idExemplaire) {
+		this.idExemplaire = idExemplaire;
+	}
+	
+	
 }
